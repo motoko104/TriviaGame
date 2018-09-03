9e42b23b0bc8e4bc2d43f4bbaf4c0069b2096f63
Manda104 <motoko104@yahoo.com>
2018-09-02 23:04:51 -0500

    basic outline for html and css laid out, start of Js

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+const questions = {
+   {
+       question: "What is the hero name of Todoroki's famous father?",
+       answer: "Endevor",
+       options: ["Midnight", "Best-Jeanist", "Kamui Woods"],
+       imageUrl: "../images/todoroki.jpg",
+       winImgUrl: "../images/endevor.jpg"
+   };
+   {
+       question: "Which leading hero started without a quirk?",
+       answer: "Midroiya",
+       options: ["Bakugo","Todoroki","Iida"],
+       imageUrl: "...images/class1A.jpg",
+       winImgUrl: "../images/midoriya.jpg"
+   };
+   {
+       question: "How did Midoriya obtain his quirk?",
+       answer: "He ate one of All Might's hairs"
+       options: ["He was born with them", "He drank a liquid that gave him powers", "he obtained a magic suit that gvie him his quirk"],
+       imageUrl: "../images/quirkless.jpg",
+       winImgUrl: "../images/eatThis.jpg"
+   },
+
+}
